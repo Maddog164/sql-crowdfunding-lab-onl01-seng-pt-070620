cfe0148a00cf5aff3ab051cb407d8a0ee688505f
Denise Henderson <denise@henderson3.us>
2020-09-07 09:36:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT Projects.title,
-   Pledges.amount-Projects.funding_goal
+   SUM(Pledges.amount)-Projects.funding_goal
    FROM Projects
    JOIN Pledges
    ON Projects.id = Pledges.project_id
